74f91d47f8fb5e97bdf87a191daf882039e99c3b
Rirurin <rirurin@rirurin.com>
2025-06-14 09:36:53 +0800

    Selectable imgui branches

--- riri-inspector-components/src/lib.rs
+++ riri-inspector-components/src/lib.rs
@@ -1,3 +1,9 @@
+#[cfg(feature = "imgui")]
+extern crate imgui_original as imgui;
+
+#[cfg(feature = "imgui-riri")]
+extern crate imgui_riri as imgui;
+
 pub mod bullet;
 pub mod panel;
 pub mod searchbar;
